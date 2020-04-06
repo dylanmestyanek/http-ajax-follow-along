aa1e15f686bf165a5078d36bfb4679638677bfb4
dylanmestyanek <mestyanekdylan@gmail.com>
2020-04-06 10:32:17 -0500

    Loading individual posts on click

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import axios from 'axios';
 
 class Blog extends Component {
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
 
     componentDidMount() {
@@ -25,9 +26,17 @@ class Blog extends Component {
             })
     }
 
+    postSelected = id => {
+        this.setState({ selectedPostId: id })
+    }
+
     render () {
         const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} author={post.author} />
+            return <Post 
+                key={post.id} 
+                title={post.title} 
+                author={post.author} 
+                clicked={() => this.postSelected(post.id)} />
         })
         return (
             <div>
@@ -35,7 +44,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
