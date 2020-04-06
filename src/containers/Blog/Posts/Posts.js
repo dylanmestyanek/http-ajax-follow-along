de25aea4f0155add5df105e2f40f45352960f0b0
dylanmestyanek <mestyanekdylan@gmail.com>
2020-04-06 16:10:46 -0500

    Converted NavLink to history.push as an alternative

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -27,7 +27,7 @@ class Posts extends Component {
     }
 
     postSelected = id => {
-        this.setState({ selectedPostId: id })
+        this.props.history.push(`/${id}`)
     }
 
     render() {
@@ -36,12 +36,13 @@ class Posts extends Component {
         if (!this.state.error){ 
             posts = this.state.posts.map(post => {
                 return (
-                    <Link to={`/${post.id}`} key={post.id}>
+                    // <Link to={`/${post.id}`} key={post.id}>
                         <Post 
+                            key={post.id}
                             title={post.title} 
                             author={post.author} 
                             clicked={() => this.postSelected(post.id)} />
-                    </Link>
+                    // </Link>
             
             )})
         }
